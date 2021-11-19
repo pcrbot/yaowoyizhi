eb552d80dcb2d5d063776ee9bd30615b2e83030e
mahosho <65062125+mahosho@users.noreply.github.com>
2021-11-19 17:09:05 +0800

    更改字体路径以适配linux

--- __init__.py
+++ __init__.py
@@ -5,12 +5,13 @@ from hoshino.util import pic2b64
 from hoshino.typing import *
 import re
 import base64
+import os
 def img_gen(pic):
     word1 = f'要我一直'
     word2 = f'吗'
     y = int(pic.size[1]*600/pic.size[0])
     y1 = int(pic.size[1]*700/pic.size[0]+30)
-    font = ImageFont.truetype('msyh.ttc', 100)
+    font = ImageFont.truetype(os.path.join(os.path.dirname(__file__),'msyh.ttc'), 100)
     im = Image.new('RGBA', (600,y1 if y1>y+150 else y+150), (255, 255, 255, 255))
     draw = ImageDraw.Draw(im)
     img1 = pic.resize((600,y), Image.ANTIALIAS)
